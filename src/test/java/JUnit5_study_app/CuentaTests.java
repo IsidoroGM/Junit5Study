a21966f3a7631a860553e163bf84f32389628e3b
Iisidoro Gordillo <igm27292@gmail.com>
2026-09-08 11:38:36 +0200

    Refactor tests in CuentaTests: renombrar métodos y mejorar la legibilidad

--- src/test/java/JUnit5_study_app/CuentaTests.java
+++ src/test/java/JUnit5_study_app/CuentaTests.java
@@ -1,25 +1,40 @@
 package JUnit5_study_app;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
 
 public class CuentaTests {
-    @Test
-    void testGetPersona() {
 
+    
+    @Test
+    void testNombreCuenta(){
+
+        //Con este test, vamos a probar setPersona y el constructor a la vez.
+
+        Cuenta cuenta=new Cuenta("Andres", new BigDecimal("1000.5655"));
+        cuenta.setPersona("Andres");
+        String esperado= "Andres";
+        String real=cuenta.getPersona();
+
+        //Assert comprueba la afirmacion de un valor
+        //En este caso, que el valor esperado y el real sean iguales.
+        assertEquals(esperado, real);
+
+        //assertTrue comprueba que un valor sea True
+        assertTrue(real.equals("Andres"));
     }
 
+    //Test para comprobar el saldo
     @Test
-    void testGetSaldo() {
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.5655"));
 
+        //probamos con assertEquals el saldo
+        assertEquals(1000.5655, cuenta.getSaldo().doubleValue());
+
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
-
-    @Test
-    void testSetPersona() {
-
-    }
-
-    @Test
-    void testSetSaldo() {
-
-    } 
 }
